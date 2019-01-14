bcb20ab9b33b93eac4b0f57721e8b54bc4fbe6aa
Igor Pronin <igor.pronin1988@gmail.com>
2019-01-14 22:11:47 +0300

    C помощью рефакторинга создали методы, тем самым повысили читаемость кода

--- test_add_group.py
+++ test_add_group.py
@@ -15,21 +15,28 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        #open home page
-        wd.get("http://localhost/addressbook/group.php")
-        #login
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        #click content
-        wd.find_element_by_id("content").click()
-        #open groups page
-        wd.find_element_by_link_text("groups").click()
-        #init group creation
+        self.open_home_page(wd)
+        self.login(wd)
+        self.click_content(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.submit_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def submit_group(self, wd):
+        wd.find_element_by_name("submit").click()
+
+    def create_group(self, wd):
+        # init group creation
         wd.find_element_by_name("new").click()
-        #fill group form
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("fhgfhg")
@@ -37,13 +44,23 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_header").send_keys("jdhajhddh")
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("dakjdkjad")
-        #submit group creation
-        wd.find_element_by_name("submit").click()
-        #return to groups page
-        wd.find_element_by_link_text("group page").click()
-        #logout
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def click_content(self, wd):
+        wd.find_element_by_id("content").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/group.php")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
