5b8e8a70fe3e4620228dec90ac9741094cbce12f
Igor Pronin <igor.pronin1988@gmail.com>
2019-02-03 14:56:57 +0300

    Повысили интеллектуальность функции Login и logout

--- Fixture/application.py
+++ Fixture/application.py
@@ -8,6 +8,7 @@ from Fixture.session import SessionHelper
 from Fixture.group import GroupHelper
 from Fixture.contact import ContactHelper
 
+
 class Application():
     def __init__(self):
         self.wd = webdriver.Firefox()
@@ -24,10 +25,9 @@ class Application():
         except:
             return False
 
-
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook/")
 
     def terminated(self):
         self.wd.quit()
