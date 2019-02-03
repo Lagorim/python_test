02c3d9a1547ada816c7e933fb8c6fe711506c519
Igor Pronin <igor.pronin1988@gmail.com>
2019-02-03 11:59:36 +0300

    Добавлен тест модифиуации группы с if-then-else

--- Fixture/group.py
+++ Fixture/group.py
@@ -41,14 +41,17 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
 
     def form_group(self, group):
+        wd = self.change_field_value("group_name", group.name)
+        wd = self.change_field_value("group_header", group.header)
+        wd = self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+        return wd
 
     def create(self, group):
         wd = self.app.wd
@@ -79,6 +82,16 @@ class GroupHelper:
         #edit group
         self.edit_group()
         self.form_group(group)
+        self.update_group()
+        self.return_to_groups_page()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.selected_group()
+        # edit group
+        self.edit_group()
+        self.form_group(new_group_data)
         self.update_group()
         self.return_to_groups_page()
 
