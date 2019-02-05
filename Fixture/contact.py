dbb54014cbbbd0f4f19ff541b48869af9396bffa
Igor Pronin <igor.pronin1988@gmail.com>
2019-02-05 23:31:12 +0300

    Добавлена интеллектуальность навигации

--- Fixture/contact.py
+++ Fixture/contact.py
@@ -7,7 +7,8 @@ class ContactHelper:
 
     def open_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def open_home_page(self):
         wd = self.app.wd
