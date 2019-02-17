f2c2b3c9464002d90c3b642eb690173b3ae87eca
Igor Pronin <igor.pronin1988@gmail.com>
2019-02-17 17:19:17 +0300

    Сравнение списков при добавление групп

--- Test/test_add_group.py
+++ Test/test_add_group.py
@@ -3,24 +3,41 @@ from Model_class.group import Group
 
 
 def test_add_group(app):
+     old_groups=app.group.get_group_list()
      app.group.create(Group(name="fhgfhg", header="jdhajhddh", footer="dakjdkjad"))
-
+     new_groups = app.group.get_group_list()
+     assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+     old_groups = app.group.get_group_list()
      app.group.create(Group(name="", header="", footer=""))
+     new_groups = app.group.get_group_list()
+     assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_igor_group(app):
+     old_groups = app.group.get_group_list()
      app.group.create(Group(name="Igor", header="Pronin", footer=""))
+     new_groups = app.group.get_group_list()
+     assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_nikas_group(app):
+     old_groups = app.group.get_group_list()
      app.group.create(Group(name="Nika's", header="Pronina's", footer="Anatoly"))
+     new_groups = app.group.get_group_list()
+     assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_nika_group(app):
+     old_groups = app.group.get_group_list()
      app.group.create(Group(name="Nika", header="Pronina's", footer="Anatoly"))
+     new_groups = app.group.get_group_list()
+     assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_nika_s_group(app):
+     old_groups = app.group.get_group_list()
      app.group.create(Group(name="Nika", header="Pronina", footer="Anatoly"))
+     new_groups = app.group.get_group_list()
+     assert len(old_groups) + 1 == len(new_groups)
